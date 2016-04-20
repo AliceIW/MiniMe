7ecf51f876c7300cb1ebc9c6d5426cddde785ad4
Alice Tagliabue <aliceiw84@gmail.com>
2016-04-20 14:28:55 +0100

    Setup bower,node and gulp for development

---
+++ dist/MiniMe.js
@@ -0,0 +1,174 @@
+/**
+ * Created by alice on 10/01/2016.
+ *
+ * Simple Jquery Framework that will allow to have a neater code.
+ */
+function MiniMeApp() {
+    "use strict";
+    var controllerFunctions = {};
+    var configFunctions = [];
+    var serviceFunctions = {};
+
+    /**
+     * Execute the configuration declared before
+     */
+    function runConfigurations() {
+        app.get('Debug').info("Running Configuration");
+        configFunctions.forEach(function (configFunc) {
+            configFunc();
+        });
+    }
+
+    /**
+     * Takes the controllers and execute them
+     */
+    function searchAndExecuteControllers() {
+        var controllersInPage = $('[data-controller]');
+        controllersInPage.each(function (i, selector) {
+            var currentController = $(selector).data('controller');
+            app.get('Debug').info("Running Controller: {" + currentController + '}');
+            if (typeof currentController !== 'undefined' && currentController in controllerFunctions) {
+                controllerFunctions['PVT_controllerName'] = currentController;
+                controllerFunctions['addEvent'] = addEventListener;
+                var params = $(selector).data('controller-params') || {};
+                controllerFunctions[currentController](params);
+            }
+        })
+    }
+
+    /**
+     *  Add an event to the specific controller
+     *
+     * @param el
+     * @param event
+     * @param fnc
+     * @returns {addEventListener}
+     */
+    function addEventListener(el, event, fnc) {
+        if ($.type(fnc) === 'string') {
+            fnc = this[fnc];
+        }
+        app.get('Debug').info("     Adding Event:  {" + event + '} on {' + el + '}');
+        $('[data-controller="' + this.PVT_controllerName + '"] ').on(event, el, fnc);
+        return this;
+    }
+
+    /**
+     * add Configuration
+     *
+     * @param func
+     * @returns {addConfiguration}
+     */
+    function addConfiguration(func) {
+        configFunctions.push(func);
+        return this;
+    }
+
+    /**
+     * Run The Framework
+     */
+    function run(debugStatus) {
+        var dbg = debugStatus || false;
+        addService('Debug', new Debug(dbg));
+        runConfigurations();
+        searchAndExecuteControllers();
+    }
+
+    /**
+     * Instantiate a new Controller
+     *
+     * @param name
+     * @param func
+     * @returns {addController}
+     */
+    function addController(name, func) {
+        controllerFunctions[name] = func;
+        return this;
+    }
+
+    /**
+     * Instantiate a new Service
+     *
+     * @param name
+     * @param func
+     * @returns {addService}
+     */
+    function addService(name, func, init) {
+        var resolve = (init == undefined) ? true : false;
+
+        if ($.type(func) == 'function' && resolve == true) {
+            serviceFunctions[name] = func();
+        } else {
+            serviceFunctions[name] = func;
+        }
+        return this;
+    }
+
+    /**
+     * Retrieve the selected service and initialise it if needed
+     *
+     * @param name
+     * @returns {*}
+     */
+    function getService(name) {
+        var args = Array.prototype.slice.call(arguments);
+
+        if ($.type(serviceFunctions[name]) == 'function') {
+            return new serviceFunctions[name](args.slice(1))
+        }
+        return serviceFunctions[name];
+    }
+
+    return {
+        version: '0.0.1',
+        service: addService,
+        get: getService,
+        controller: addController,
+        config: addConfiguration,
+        run: run
+    };
+}
+
+app = new MiniMeApp();
+/**
+ * Created by alice on 21/01/2016.
+ *
+ * Debug Service
+ */
+
+function Debug(debugStatus) {
+    var enableDebug = debugStatus || false;
+    this.log = function () {
+        if (enableDebug) {
+            console.info.apply(console, arguments);
+        }
+    }
+
+    this.info = function () {
+        var newArguments = colourise(arguments);
+
+        if (newArguments == null) {
+            newArguments = arguments;
+        }
+        if (enableDebug) {
+            console.info.apply(console, newArguments);
+        }
+    };
+
+    function colourise(arguments) {
+        var message = arguments[0];
+        var newArgumentsArray = [""];
+        var matches = message.match(/{(.*?)}/g);
+        if (matches === null) {
+            return null;
+        }
+        matches.forEach(function (match) {
+            message = message.replace(match, '%c' + match.substr(1, match.length - 2) + '%c');
+            newArgumentsArray.push('color:#0074D9;font-weight:bold');
+            newArgumentsArray.push('')
+        });
+        newArgumentsArray[0] = message;
+
+        return newArgumentsArray;
+    }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,21 @@
 var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     rev = require('gulp-rev'),
     notify = require('gulp-notify'),
-    batch = require('gulp-batch'),
-    bower = require('gulp-bower'),
     jshint = require('gulp-jshint'),
     concat = require('gulp-concat'),
-    sass = require('gulp-ruby-sass'),
     uglify = require('gulp-uglify'),
     watch = require('gulp-watch'),
     rimraf = require('gulp-rimraf');
 
 gulp.task('clean-js', function () {
-    return gulp.src('public/js/main-*.js', {read: false})
+    return gulp.src('dist/*.js', {read: false})
         .pipe(rimraf({force: true}))
         .pipe(notify({message: 'Files Deleted'}));
 });
 
 gulp.task('jshint',function(){
-    return gulp.src('resources/assets/scripts/**/*.js')
+    return gulp.src('src/**/*.js')
         // if flag is not defined default value is 'auto'
         .pipe(jshint.extract('auto'))
         .pipe(jshint())
@@ -28,23 +23,19 @@ gulp.task('jshint',function(){
 });
 
 gulp.task('scripts', ['clean-js'], function () {
-    return gulp.src('resources/assets/scripts/**/*.js')
+    return gulp.src('src/**/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
         .pipe(rev())
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('public/assets/js'))
+        .pipe(concat('MiniMe.js'))
+        .pipe(gulp.dest('dist'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('public/assets/js'))
+        .pipe(gulp.dest('dist'))
         .pipe(notify({message: 'Scripts task complete'}));
 });
 
-gulp.task('bower', function() {
-    return bower()
-        .pipe(gulp.dest('/'))
-});
 
 gulp.task('watch', function () {
-    gulp.watch("resources/assets/scripts/**/*.js", ["scripts"]);
+    gulp.watch("scr/**/*.js", ["scripts"]);
 });
